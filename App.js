5c5b7c3a8ff80e8bfce16e0d9d94e3c0eab9427b
Yashi Bagoli <y.bagoli32@gmail.com>
2023-11-16 00:29:53 +0530

    Episode-3 Diff b/w React.createElement & JSX

--- App.js
+++ App.js
@@ -1,45 +1,19 @@
-/** 
-* Nested Elements in React
-* Siblings within a div,use array to store them.
-* ReactElement(Object) ====>> HTML(Browser Understands) 
-
-
-<div id="parent">                           
-    <div id="child1">                   ------> child of div id=parent
-            <h1>I'm h1 tag</h1>         ------> children of div id=child1
-            <h1>I'm a h1 tag</h1>       ------> children of div id=child1
-    </div>
-
-    <div id="child2">                   ------> child of div id=parent
-            <h1>I'm h2 tag</h1>         ------> children of div id=child2
-            <h1>I'm a h2 tag</h1>       ------> children of div id=child2
-    </div>
-</div>
-
-*/
-
-
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const parent= React.createElement(
-    "div",
- { id:"parent" },
- [
-    React.createElement(
-    "div",
-    {id:"child1"},
-    [React.createElement("h1",{},"Im a h1 tag"),React.createElement("h1",{},"I'm a h1 tag")] 
-    ),
-    React.createElement(
-        "div",
-        {id:"child2"},
-        [React.createElement("h2",{},"Im a h2 tag"),React.createElement("h2",{},"I'm a h2 tag")] 
-        )
-    ]
-    );
+// React.createElement ==> React Element (JS Object) ==> HTML Element(render)
+const heading=React.createElement(
+    "h1",
+    { id:"heading" },
+    "Namaste React🚀"
 
-const root= ReactDOM.createRoot(document.getElementById("root"));        
-
-root.render(parent);       
+)
+// JSX ==> React.createElement ==>ReactElement(Object) ==>HTML Element (render)
+// JSX(transpiled before it reaches JS)- PARCEL- BABEL
+const jsxheading= <h1 id="heading"> Namaste React Using JSX 🚀 </h1>
+const root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+console.log(jsxheading);
+root.render(jsxheading);
+ 
 
