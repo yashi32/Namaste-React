bc9b0b4a33767197353d2845c484c9bac9111f30
Yashi Bagoli <y.bagoli32@gmail.com>
2023-12-09 21:08:25 +0530

    React Hooks: useState() setvariable

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,36 @@
 import RestroCart from "./RestroCart";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 
 const Body=()=>{
+
+//React Hooks
+//State variable -Superpower variable
+  let [restaurantList,setrestaurantList]=useState(resList);
+  console.log(restaurantList);
     return(
         <div className="body">
-            <div className="search">Seach</div>
+            <div className="filter-btn">
+              <button onClick={()=>{
+                //Filter Logic
+                filteredData=restaurantList.filter(
+                  (res)=>res.info.avgRating>4
+                )
+               
+                setrestaurantList(filteredData);
+                console.log(restaurantList)
+              }
+              
+            }
+              > 
+              Search Top Restaurants
+              </button>
+              </div>
             <div className="restro-container">
                 {
                   //Using map function to iterate over list item
-                  resList.map((x,index)=>
+                  restaurantList.map((x,index)=>
                   (
                     <RestroCart key={x.info.id} resData={x}/>
                   ))
